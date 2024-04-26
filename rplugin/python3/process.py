01fab00d7d29645be290cf8d0bf3ab0b26e1793c
RajaRaviVarma <rajaravivarma.ar@team.wrike.com>
2024-04-27 02:28:11 +0530

    Add popen_and_get_output to process.py

--- rplugin/python3/process.py
+++ rplugin/python3/process.py
@@ -5,6 +5,14 @@ def execute_command(command):
     subprocess.run(command)
 
 
+def popen_and_get_output(command: list, stdin_input: str):
+    process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    stdout, stderr = process.communicate(input=stdin_input.encode("utf-8"))
+
+    # Decode to string
+    return stdout.decode("utf-8")
+
+
 def execute_and_get_output(command, as_string=True):
     output = subprocess.check_output(command)
     if as_string:
