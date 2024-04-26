2c710f5db2e5a7818334fef2eeec4e2ca38e2a3e
RajaRaviVarma <rajaravivarma.ar@team.wrike.com>
2024-04-27 02:27:53 +0530

    Format fzf.py

--- rplugin/python3/fzf.py
+++ rplugin/python3/fzf.py
@@ -1,9 +1,12 @@
 import process
 
+
 class Fzf:
     def __init__(self, executable="fzf"):
         self.executable = executable
 
-    def filter(self, string):
-        output = process.execute_and_get_output([self.executable, '--filter', string])
+    def filter(self, filter_string, input_string):
+        output = process.popen_and_get_output(
+            [self.executable, "--filter", filter_string], input_string
+        )
         return output.splitlines()
