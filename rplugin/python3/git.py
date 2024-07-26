0ef574dac346f38153a009f10da6ec68b62ccc7d
RajaRaviVarma AR <rajaravivarma.ar@gmail.com>
2024-07-26 15:44:51 +0530

    Strip the output of git get_root_directory return value

--- rplugin/python3/git.py
+++ rplugin/python3/git.py
@@ -1,4 +1,7 @@
 import process
 
+
 def get_root_directory():
-    return process.execute_and_get_output(['git', 'rev-parse', '--show-toplevel'])
+    return process.execute_and_get_output(
+        ["git", "rev-parse", "--show-toplevel"]
+    ).strip()
