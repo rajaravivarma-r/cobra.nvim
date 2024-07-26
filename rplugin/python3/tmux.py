c9f0da3dfdb1d6dcac2efcdef9c24a95a8ed918c
RajaRaviVarma AR <rajaravivarma.ar@gmail.com>
2024-07-26 15:44:50 +0530

    Add method to return window_index.pane_index in tmux

--- rplugin/python3/tmux.py
+++ rplugin/python3/tmux.py
@@ -11,6 +11,10 @@ def run_command(command, target_pane):
     send_keys(command, target_pane)
     send_keys(ControlKey.ENTER.value, target_pane)
 
+def get_window_index_and_pane_index_for_current_session(separator='.'):
+    command = ['tmux', 'list-panes', '-s', '-F', '#{window_index}.#{pane_index}']
+    output = process.execute_and_get_output(command)
+    return [line.strip() for line in output.strip().splitlines()]
 
 def send_keys(text, target_pane):
     command = ["tmux", "send-keys", f"-t:{target_pane}", text]
